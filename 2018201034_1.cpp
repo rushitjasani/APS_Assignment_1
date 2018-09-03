a6e0bbeed2873d8ae5931f6a56a161f2b0ec21e2
rushitjasani <rushitjasani@gmail.com>
2018-09-04 00:08:47 +0530

    BigInteger Completed.

--- 2018201034_1.cpp
+++ 2018201034_1.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
-#define ll long long;
+#define ll long long
 
 class BigInteger
 {
@@ -15,28 +15,21 @@ class BigInteger
     BigInteger(){};
     BigInteger(string input_data)
     {
-        if (input_data[0] == '+')
-        {
-            sign = 0;
-            size = input_data.length() - 1;
-            data = input_data.substr(1, size);
-            data.erase(0, min(data.find_first_not_of('0'), data.size() - 1));
-            if(data.length() == 0) data += "0";
-            size = data.length();
-        }
-        else if (input_data[0] == '-')
+        if (input_data[0] == '-')
         {
             sign = 1;
             size = input_data.length() - 1;
             data = input_data.substr(1, size);
             data.erase(0, min(data.find_first_not_of('0'), data.size() - 1));
-            if(data.length() == 0) data += "0";
+            if (data.length() == 0)
+                data += "0";
             size = data.length();
         }
         else
         {
             input_data.erase(0, min(input_data.find_first_not_of('0'), input_data.size() - 1));
-            if(input_data.length() == 0) input_data += "0";
+            if (input_data.length() == 0)
+                input_data += "0";
             sign = 0;
             size = input_data.length();
             data = input_data;
@@ -116,7 +109,8 @@ class BigInteger
     }
     string tenscomp(string s)
     {
-        if(s.length() == 1 && s[0] == '0') return s;
+        if (s.length() == 1 && s[0] == '0')
+            return s;
         for (unsigned int i = 0; i < s.length(); i++)
         {
             s[i] = '9' - s[i] + '0';
@@ -140,7 +134,8 @@ class BigInteger
     }
     BigInteger subtract(BigInteger x, BigInteger y)
     {
-        if(y.size == 1 && y.data[0] == '0') return x;
+        if (y.size == 1 && y.data[0] == '0')
+            return x;
         if (x.sign == 0 && y.sign == 0)
         {
             int carry_val = 0;
@@ -253,9 +248,7 @@ class BigInteger
             else
             {
                 BigInteger addition_helper(one_digit_mul);
-                cout << "old_val:: " << mult->data << "oneline : " << one_digit_mul << endl;
                 BigInteger part_sum = mult->add(addition_helper);
-                cout << "part_sum :: " << part_sum.data << endl;
                 *mult = part_sum;
             }
         }
@@ -265,23 +258,100 @@ class BigInteger
         }
         return (*mult);
     }
+    BigInteger divide(BigInteger x)
+    {
+        BigInteger one("1");
+        BigInteger ans("0");
+        int sign;
+        if (this->data[0] == 0 && x.data[0] == 0)
+        {
+            return BigInteger("0");
+        }
+        else
+        {
+            if (this->sign != x.sign)
+            {
+                sign = 1;
+            }
+            x.sign = 0;
+            this->sign = 0;
+            while (1)
+            {
+                BigInteger tmp = subtract(*this, x);
+                this->data = tmp.data;
+                this->sign = tmp.sign;
+                this->size = tmp.size;
+                if (tmp.sign == 1)
+                    break;
+                tmp = add(ans, one);
+                ans.data = tmp.data;
+                ans.sign = tmp.sign;
+                ans.size = tmp.size;
+            }
+            if (sign == -1)
+            {
+                ans.sign = sign;
+            }
+            return ans;
+        }
+    }
+    bool isEqual(BigInteger x, BigInteger y)
+    {
+        if (x.data == y.data && x.sign == y.sign && x.size == y.size)
+            return true;
+        return false;
+    }
+    BigInteger my_gcd(BigInteger x)
+    {
+        BigInteger zero("0");
+        if (isEqual(*this, zero))
+            return x;
+        else
+        {
+            BigInteger divisoin, mult, sub, res, a, b;
+            a = *this;
+            b = x;
+            divisoin = b.divide(a);
+            mult = divisoin.multiply(a);
+            b = x;
+            sub = b.subtract(mult);
+            return sub.my_gcd(a);
+        }
+    }
 };
 
 int main()
 {
-    while (1)
+    ll t;
+    cin >> t;
+    while (t--)
     {
         string x, y;
-        cin >> x >> y;
+        int m;
+        cin >> x >> y >> m;
         BigInteger b1(x);
         BigInteger b2(y);
-        BigInteger c, d, e;
-        e = b1.multiply(b2);
-        c = b1.add(b2);
-        d = b1.subtract(b2);
-        cout << c.getData() << endl;
-        cout << d.getData() << endl;
-        cout << e.getData() << endl;
+        BigInteger ans;
+        switch (m)
+        {
+        case 1:
+            ans = b1.add(b2);
+            ;
+            break;
+        case 2:
+            ans = b1.subtract(b2);
+            break;
+        case 3:
+            ans = b1.multiply(b2);
+            break;
+        case 4:
+            ans = b1.divide(b2);
+            break;
+        case 5:
+            ans = b1.my_gcd(b2);
+            break;
+        }
+        cout << ans.getData() << endl;
     }
     return 0;
 }
